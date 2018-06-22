6b1645f8d33736cb77eb87aa8183e5d689c6daff
vrcorpru <head@vrcorp.ru>
2018-06-22 19:15:18 +0700

    no message

---
+++ Assets/Scripts/Planets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planets : MonoBehaviour {
+
+    string planetName = "Земля";
+    int radius = 6371;
+    float rotationSpeed = 0.46f;
+
+    // Use this for initialization
+    void Start () {
+        transform.rotation = Quaternion.Euler(10, 90, 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, rotationSpeed*Time.deltaTime, 0);
+	}
+}
---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Spaceship : MonoBehaviour {
+    
+    float speed;
+    public GameObject vrCamera;
+    Vector3 forward;
+
+    // Use this for initialization
+    void Start () {        
+        speed = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        forward = vrCamera.transform.forward * speed * Time.deltaTime;
+        transform.Translate(forward);
+    }
+}
